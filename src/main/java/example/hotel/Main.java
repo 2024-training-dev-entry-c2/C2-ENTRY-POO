0ab2789d28904d142be3cc788259d7c3fb9a4721
Nahuel Lemes <nahulem@gmail.com>
2024-12-15 12:41:32 -0300

    feat (developement):  add parallel array of hotels
    Implemented initial data structure for storing hotel data using parallel arrays.

--- src/main/java/example/hotel/Main.java
+++ src/main/java/example/hotel/Main.java
@@ -2,6 +2,19 @@ package example.hotel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // hoteles
+        String hotelNombres[] = {"Hotel Buenos Aires", "Hotel Mar del Plata", "Apartamento Cordoba", "Finca Rosario", "Dia de Sol Buenos Aires", "Dia de Sol Mar del Plata"};
+        String tipoAlojamiento[] = {"Hotel", "Hotel", "Apartamento", "Finca", "Dia de Sol", "Dia de Sol"};
+        String ciudades[] = {"Buenos Aires", "Mar del Plata", "Cordoba", "Rosario", "Buenos Aires", "Mar del Plata"};
+        int[] calificaciones = {4, 3, 2, 5, 4, 3};
+        double precios[] = {100, 80, 50, 150, 120, 100};
+        String[][] hotelFechasDisponibles = {
+                {"2024-12-01", "2024-12-10"},
+                {"2024-12-05", "2024-12-15"},
+                {"2024-12-20", "2024-12-30"},
+                {"2024-12-01", "2024-12-10"},
+                {"2024-12-05", "2024-12-15"},
+                {"2024-12-20", "2024-12-30"}
+        };
     }
 }
