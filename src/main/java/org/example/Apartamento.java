f4c141281d5a31a9bd8dd9e93e430d5acba8bdcd
Marty Parada <martin.p1131@gmail.com>
2024-12-17 08:57:16 -0500

    refact(development): add new variables

--- src/main/java/org/example/Apartamento.java
+++ src/main/java/org/example/Apartamento.java
@@ -11,7 +11,17 @@ public class Apartamento extends Alojamiento {
         this.perro = perro;
     }
 
-    boolean perro;
+    protected boolean perro;
+
+    public boolean isParqueadero() {
+        return parqueadero;
+    }
+
+    public void setParqueadero(boolean parqueadero) {
+        this.parqueadero = parqueadero;
+    }
+
+    protected  boolean parqueadero;
 
     public Apartamento(String nombre, String ciudad, String tipo, double calificacion, List<Habitacion> habitaciones) {
         super(nombre, ciudad, tipo, calificacion, habitaciones);
