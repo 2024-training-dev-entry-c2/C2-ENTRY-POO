4f0df85c0a490dd33ca3d60bff8abaaba9985dad
Johan <johanvilla64@gmail.com>
2024-12-16 22:45:11 -0500

    feat: :sparkles: Add method to insert rooms to acomodation obj

--- src/Alojamiento.java
+++ src/Alojamiento.java
@@ -23,4 +23,8 @@ public class Alojamiento {
         return habitaciones;
     }
 
+    public void setHabitaciones(ArrayList<Habitacion> habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
 }
