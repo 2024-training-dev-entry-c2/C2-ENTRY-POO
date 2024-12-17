4f2dda3ace610d049d7cb22836258bfdee1d5936
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2024-12-16 20:49:28 -0500

    feat(reserva-cliente): se agrego la logica para las clases cliente y reserva

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -1,6 +1,79 @@
 package src.clientes;
 
 public class Cliente {
-    
 
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String nacionalidad;
+    private String telefono;
+    private String horaLlegada;
+    private String fechaNacimiento;
+
+
+    public Cliente(String nombre, String apellido, String email, String nacionalidad, String telefono, String horaLlegada, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.nacionalidad = nacionalidad;
+        this.telefono = telefono;
+        this.horaLlegada = horaLlegada;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getHoraLlegada() {
+        return horaLlegada;
+    }
+
+    public void setHoraLlegada(String horaLlegada) {
+        this.horaLlegada = horaLlegada;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
--- src/reservas/Reserva.java
+++ src/reservas/Reserva.java
@@ -1,7 +1,68 @@
 package src.reservas;
 
+import src.clientes.Cliente;
+import src.habitaciones.Habitacion;
+
+import java.time.LocalDate;
+
 public class Reserva {
 
+    private Cliente cliente;
+    private Habitacion habitacion;
+    private int cantHabitaciones;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
 
 
+    public Reserva(Cliente cliente, Habitacion habitacion, int cantHabitaciones, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.cliente = cliente;
+        this.habitacion = habitacion;
+        this.cantHabitaciones = cantHabitaciones;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public void mostrarInfoReserva(){
+        
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public int getCantHabitaciones() {
+        return cantHabitaciones;
+    }
+
+    public void setCantHabitaciones(int cantHabitaciones) {
+        this.cantHabitaciones = cantHabitaciones;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
