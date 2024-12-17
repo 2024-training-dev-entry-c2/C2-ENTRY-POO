331e9e269f888b815126df2c88dbf22f1fd139dd
Johan <johanvilla64@gmail.com>
2024-12-17 03:02:40 -0500

    feature: :construction: Overriding consrtuctor of Reserva and add reservation-type-attribute

--- src/Reserva.java
+++ src/Reserva.java
@@ -10,6 +10,7 @@ public class Reserva {
     public Cliente cliente;
     public Alojamiento alojamiento;
     public Habitacion habitacion;
+    public String tipo;
 
     public Reserva(LocalDate fechaInicio, LocalDate fechaFin, int cantidadAdultos, int cantidadNinos, Cliente cliente, Alojamiento alojamiento, Habitacion habitacion) {
         this.fechaInicio = fechaInicio;
@@ -19,10 +20,28 @@ public class Reserva {
         this.cliente = cliente;
         this.alojamiento = alojamiento;
         this.habitacion = habitacion;
+        this.tipo = "Normal";
+    }
+
+    public Reserva  (LocalDate fechaInicio, int cantidadAdultos, int cantidadNinos, Cliente cliente, Alojamiento alojamiento) {
+        this.fechaInicio = fechaInicio;
+        this.cantidadAdultos = cantidadAdultos;
+        this.cantidadNinos = cantidadNinos;
+        this.cliente = cliente;
+        this.alojamiento = alojamiento;
+        this.habitacion = new Habitacion("Sin habitación", "Habitación por defecto", 0, 0);
+        this.tipo = "Día de sol";
     }
 
     public String mostrarDetalles() {
-        return "Reserva: " + "Fecha de inicio: " + fechaInicio + ", Fecha de fin: " + fechaFin + ", Cantidad de adultos: " + cantidadAdultos + ", Cantidad de niños: " + cantidadNinos + ", Cliente: " + cliente.nombre + " " + cliente.apellido + ", Alojamiento: " + alojamiento.nombre + ", Habitación: " + habitacion;
+        return "Reserva de tipo " + tipo + " : " + "Fecha de inicio: " + fechaInicio + ", Fecha de fin: " + fechaFin + ", Cantidad de adultos: " + cantidadAdultos + ", Cantidad de niños: " + cantidadNinos + ", Cliente: " + cliente.nombre + " " + cliente.apellido + ", Alojamiento: " + alojamiento.nombre + ", Habitación: " + habitacion.nombre;
+    }
+
+    public float calcularCostoReserva(LocalDate fechaInicio, LocalDate fechaFin, float precioNoche) {
+        float costoReserva = 0;
+        int dias = fechaInicio.until(fechaFin).getDays();
+        costoReserva = dias * precioNoche;
+        return costoReserva;
     }
 
 }
