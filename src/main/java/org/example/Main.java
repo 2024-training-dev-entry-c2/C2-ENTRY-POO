baaabaedf5e0965908f7f50977d631f23e86387f
Marty Parada <martin.p1131@gmail.com>
2024-12-18 14:42:14 -0500

    Sync Fork

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Creamos la instancia del sistema de reservas
         BookingSystem bookingSystem = new BookingSystem();
-
-        // Iniciamos el sistema de reservas
         bookingSystem.iniciarReserva(scanner);
-
         scanner.close();
     }
 }
